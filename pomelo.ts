b1f451aa571cce06859212ed20fd1006d70fad7c
DingGuofeng <dingguofeng@taoziyouxi.net>
2018-11-29 14:33:00 +0800

    remove ts

--- pomelo.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "./lib/client";
-
-export namespace pomelo {
-    export function create(uri: string, opts: any): Client {
-        const c = new Client(uri);
-        c.connect(opts);
-        return c;
-    };
-}
